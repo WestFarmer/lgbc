f1c915ac13f00d944913d81817a80da8dd2473af
WestFarmer <west_farmer@163.com>
2019-08-26 21:33:43 +0800

    initial commit

---
+++ entry.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/lgbc/lang"
+)
+
+func main() {
+	lang.PrintIntRange()
+}
---
+++ lang/variable.go
@@ -0,0 +1,10 @@
+package lang
+
+import (
+	"fmt"
+	"math"
+)
+
+func PrintIntRange() {
+	fmt.Printf("int32[%v, %v]", math.MinInt32, math.MaxInt32)
+}
